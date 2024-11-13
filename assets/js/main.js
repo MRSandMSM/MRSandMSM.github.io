30c0fc1de81e22f28fb259b294033229f9135cd9
MRSandMSM <MRSandMSM@gmail.com>
2024-11-13 19:28:36 -0300

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  /* I. Setting global variables */
+  var $window = $(window)
+  var $document = $("html")
+  var $body = $("body")
+  var $container = $("#pageContainer")
+  var $header = $("#header")
+})
