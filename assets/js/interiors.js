f25e6cfbe6746c752da45d810ea8414e34f32eae
MRSandMSM <MRSandMSM@gmail.com>
2024-11-17 19:49:52 -0300

    Update interiors.js

--- assets/js/interiors.js
+++ assets/js/interiors.js
@@ -12,8 +12,14 @@ $(document).ready(function(){
       var trackColors = $loadedTracklist.gradientStops("#052d6a", "#c91e30", tracksQty);
 
       $loadedTracklist.find("li").each(function(trackN){
-        $(this).css("color", trackColors[trackN]);
-        $(this).find(".title").before('<span class="track">' + parseInt(trackN + 1) + '</span>')
+        var $this = $(this);
+        $this.css("color", trackColors[trackN]);
+        $this.find(".title").before('<span class="track">' + parseInt(trackN + 1) + '</span>')
+        
+        var $trackTitle = $this.find(".title");
+        var titleArray = $trackTitle.text().split(" ").join("-");
+        var lyricsLink = '<a href="/lyrics/' + titleArray + '" class="lyrics"></a>';
+        $trackTitle.after(lyricsLink)
       });
     })
   })
