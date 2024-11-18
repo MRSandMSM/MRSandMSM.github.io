f8bc0e10e8dfc05d35c578cbc012f4ee6df861d7
MRSandMSM <MRSandMSM@gmail.com>
2024-11-17 23:20:44 -0300

    Update interiors.js

--- assets/js/interiors.js
+++ assets/js/interiors.js
@@ -31,7 +31,8 @@ $(document).ready(function(){
           var $this = $(this);
           $this.toggleClass("playing");
           $this.parent().siblings().find("a.play").removeClass("playing");
-          $this.closest(".release, .release-type").siblings().find("a.play").removeClass("playing");
+          $this.closest(".release-type").css("opacity",".5")
+          // $this.closest(".release, .release-type").siblings().find("a.play").removeClass("playing");
 
           $audioTrack = $this.closest(".tabs").find("audio")
           $audioTrack.attr("src", $trackPlay.attr("href"))
