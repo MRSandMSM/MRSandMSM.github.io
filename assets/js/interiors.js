b8f4e61d3fbff0d334c57164f42b6e4557f19027
MRSandMSM <MRSandMSM@gmail.com>
2024-11-24 11:57:34 -0300

    Update interiors.js

--- assets/js/interiors.js
+++ assets/js/interiors.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
           // $this.toggleClass("playing");
           // $this.parent().siblings().find($audioURLSelector).toggleClass("playing")
           alert($audioURLSelector + ": " +  $($audioURLSelector).length)
-          $($audioURLSelector).toggleClass("playing")
+          $($audioURLSelector).css("color", "red").toggleClass("playing")
           $this.parent().siblings().find("a.play").not($audioURLSelector).removeClass("playing");
           $this.closest(".tracklist, .release, .release-type").siblings().find("a.play").removeClass("playing");
 
