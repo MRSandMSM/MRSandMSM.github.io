6bd827433e232e4736b90dab85e7456c15b505ed
MRSandMSM <MRSandMSM@gmail.com>
2024-12-29 19:54:33 -0300

    Update interiors.js

--- assets/js/interiors.js
+++ assets/js/interiors.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     $this.find(".tabs").append("<audio></audio>")
 
     $(window).on("load resize", function(){
-      var winW = $(window).outerWidth(true);
+      var winW = $(window).innerWidth(true);
       if(winW > 486 && winW <= 767) {
         $this.find(".artwork-img").prependTo($this.find(".header"))
       } else {
