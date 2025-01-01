3336aa8c41647b3163846d291ee77923659f1874
MRSandMSM <MRSandMSM@gmail.com>
2025-01-01 05:06:29 -0300

    Update interiors.js

--- assets/js/interiors.js
+++ assets/js/interiors.js
@@ -116,7 +116,7 @@ $(document).ready(function(){
   /* V. 404 page */
   $(".not-found").each(function(){
     var $this = $(this);
-    $this.addClass("loaded").delay(375).find(".text404-inner").removeClass("pointer-disabled")
+    $this.addClass("loaded").delay(750).find(".text404-inner").removeClass("pointer-disabled")
     $this.find('.text404-inner a.toaster[href="#search"]').toaster({
       delay: 375,
       after: function(){
