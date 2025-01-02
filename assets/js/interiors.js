2298e1d1c945de2124df8af0a809d46dac33ce4e
MRSandMSM <MRSandMSM@gmail.com>
2025-01-01 21:19:08 -0300

    Update interiors.js

--- assets/js/interiors.js
+++ assets/js/interiors.js
@@ -167,7 +167,7 @@ $(document).ready(function(){
         $this.siblings(".clear").fadeIn(375, "linear");
       } else {
         $label.removeClass("floating-active").addClass("floating-inactive");
-        $this.siblings(".clear").hide(375, "linear");
+        $this.siblings(".clear").fadeIn(375, "linear");
       }
     })
   })
